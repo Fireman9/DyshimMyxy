135903b39f99346df610a9bb1014371f0ddf6e3a
AntonFiveis <53583297+AntonFiveis@users.noreply.github.com>
2020-01-14 22:53:16 +0200

    correct file

--- задачаС3И5/Source1.cpp
+++ задачаС3И5/Source1.cpp
@@ -1,21 +1,34 @@
 #include "Header.h"
-std::string answer(int n) {
-	std::string ans = "1";
-	while (n > 1) {
-		if (n % 3 != 0)
-		{
-			n -= 5;
-			ans = '(' + ans;
-			ans += "+5)";
+#include <vector>
+#include <iostream>
+#include <cmath>
+using namespace std;
+string answer(int n) {
+	int k3;
+	int k5=-1;
+	string ans="(1)";
+	for (int i = 0; pow(3, i) <= n; i++) {
+		if ((n - (int)pow(3, i )) % 5 == 0) {
+			k5 = (n - (int)pow(3, i)) / 5;
+			k3 = i;
+			//break;
 		}
-		else {
-			n /= 3;
-			ans = '(' + ans;
-			ans += "*3)";
-		}
+		cout << k3 << endl;
 	}
-	if (n == 1)
-		return ans;
-	else
-		return "None";
+
+	if (k5==-1)
+		return "none";
+	for (int i = 0; i < k3; i++) {
+		ans = '(' + ans;
+		if (k5 >= pow(3, k3 - i)) {
+			ans = '(' + ans + "+5)";
+			k5 -= pow(3, k3 - i);
+		}
+		ans += "*3)";	
+	}
+	for (int i = 0; i < k5; i++) {
+		ans = '(' + ans + "+5)";
+	}
+	//cout << k3<<' '<<k5;
+	return ans;
 }
