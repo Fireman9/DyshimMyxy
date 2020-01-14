90e146a92f483c2a3a99f316ab3e63265918fd33
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2020-01-14 19:30:43 +0200

    Create proc.h

---
+++ SnailStep/proc.h
@@ -0,0 +1,6 @@
+#pragma once
+int** create_matr(int m);
+void del_matr(int** matr, int m);
+void gener_matr(int** matr, int m);
+void print_matr(int** matr, int m);
+void snailstep(int** matr, int m);
