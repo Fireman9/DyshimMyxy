c052ebcacbdd289456ea9044e49d1786fda7aa8f
jprorokova <54396978+jprorokova@users.noreply.github.com>
2020-01-15 22:21:27 +0200

    Create Header.h

---
+++ MatrixAndStrings/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+int** CreateMatrix(int m, int n);
+void GenerateMatrix(int** matrix, int m, int n);
+int** CreateArray(int** matrix, int m, int n);
+int** ChangingArray(int** arr_string, int m, int n);
+void outputMatrix(int** matrix, int m, int n);
+char** ToStringArray(int**, int, int);
+
+char** Sort(char**, int n);
